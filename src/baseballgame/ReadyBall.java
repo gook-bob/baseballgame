ed258cf6d84c68ac452259962d438a6fc8ec078f
gook-bob <starrla1234@gmail.com>
2023-04-01 15:48:55 +0900

    플레이어 컴퓨터 숫자 비교 메소드

--- src/baseballgame/ReadyBall.java
+++ src/baseballgame/ReadyBall.java
@@ -6,20 +6,23 @@ public class ReadyBall {
     private int BALL_SIZE = 3;
     private int[] comNum = new int[BALL_SIZE];
     private int[] playerNum = new int[BALL_SIZE];
+    private int strike;
+    private int ball;
 
-    /*랜덤으로 생성하는 메소드*/
+    /*랜덤으로 생성하는 메소드 23.04.01*/
     public void comBall() {
         while (true) {
             for (int i = 0; i < BALL_SIZE; i++) {
                 comNum[i] = (int) (Math.random() * 10);
             }
             if (!(comNum[0] == comNum[1] || comNum[0] == comNum[2] || comNum[1] == comNum[2])) {
-                System.out.printf("%d %d %d", comNum[0], comNum[1], comNum[2]);
+                System.out.printf("%d %d %d\n", comNum[0], comNum[1], comNum[2]);
                 break;
             }
         }
     }
 
+    /*플레이어 숫자 입력 23.04.01*/
     public void playerBall () {
         int input = scan.nextInt();
         int myX = input / 100;
@@ -31,4 +34,18 @@ public class ReadyBall {
         }
             System.out.printf("\n%d %d %d", myX, myY, myZ);
         }
+
+    /*숫자 비교후 출력*/
+    public void checkBall(){
+        for (int i = 0; i < BALL_SIZE; i++) {
+              if (playerNum[i] == comNum[i]) {
+                    strike++;}
+              for (int z = 0; z < BALL_SIZE; z++) {
+                    if (playerNum[i] != comNum[i] && playerNum[i] == comNum[z]) {
+                        ball++;
+                    }
+                }
+            }
+        System.out.printf("\n%d스트라이크\n%d볼", strike,ball);
+    }
 }
